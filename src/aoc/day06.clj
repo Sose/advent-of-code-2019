883f51f8c974d5fbca57c03dd10a729a4f6b71a5
Joni Hiltunen <djonih@gmail.com>
2019-12-08 09:29:50 +0200

    Day 6 part 2

--- src/aoc/day06.clj
+++ src/aoc/day06.clj
@@ -14,7 +14,9 @@ G)H
 D)I
 E)J
 J)K
-K)L")
+K)L
+K)YOU
+I)SAN")
 
 (defn orbits
   "Orbits as hashmap"
@@ -37,3 +39,49 @@ K)L")
   (-> input
       (orbits)
       (count-all-edges "COM" 0)))
+
+;; part 2
+
+(defn find-val
+  [orbits tofind]
+  (->> orbits
+       (filter (fn [[k v]] (get v tofind)))
+       first
+       first))
+
+(defn route-from-to
+  [orbits from to]
+  (loop [from from
+         result '()]
+    (if (= from to)
+      result
+      (let [found (find-val orbits from)]
+        (recur found
+               (conj result found))))))
+
+;;(route-from-to orbits "SAN" "COM")
+
+(defn first-common-orbit
+  [route1 route2]
+  (loop [route1 route1
+         depth 0]
+    (if-let [found (some #{(first route1)} route2)]
+      (list found depth)
+      (recur (rest route1)
+             (inc depth)))))
+
+
+(defn part2
+  []
+  (let [orbits (orbits input)
+        santaroute (reverse (route-from-to orbits "SAN" "COM"))
+        youroute (reverse (route-from-to orbits "YOU" "COM"))]
+    (+ (second (first-common-orbit santaroute youroute))
+       (second (first-common-orbit youroute santaroute)))))
+
+;; main
+
+(defn main
+  []
+  (println "P1:" (part1))
+  (println "P2:" (part2)))
